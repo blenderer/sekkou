d355183b366ca3e6c44691b9738bff79ef971d2f
Eric Harrison <blendererer@gmail.com>
2013-08-19 16:47:30 -0400

    added a test file to control things

--- Floor.lua
+++ Floor.lua
@@ -8,4 +8,6 @@ Floor = class('Floor')
 function Floor:initialize(room_count, room_interface)
 	self.n = room_count
 	self.ri = room_interface
+
+	self.tiles = Array2d:new()
 end
---
+++ test.lua
@@ -0,0 +1,4 @@
+require 'Floor'
+
+dungeon = Floor:new(2, {{w=5, h=6}})
+
