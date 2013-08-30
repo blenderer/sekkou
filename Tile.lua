0b74833f1aca395748181ffb60729ecf5f5891c6
Eric Harrison <blendererer@gmail.com>
2013-08-30 16:05:28 -0400

    added tagging to the Tile class

--- Tile.lua
+++ Tile.lua
@@ -4,8 +4,27 @@ function Tile:initialize(col, row, grid, tipe)
 	self.x = col
 	self.y = row
 	self.grid = grid
+
+	self.tags = {}
+
 	self.room = nil
 	self.type = tipe or "r"
+end
+
+function Tile:addTag(newtag)
+	table.insert(self.tags, newtag)
+end
+
+function Tile:hasTag(searchtag)
+	found = false
+	for i = 1, #self.tags do
+		if self.tags[i] == searchtag then
+			found = true
+			break
+		end
+	end
+
+	return found
 end
 
 function Tile:getLeft()
